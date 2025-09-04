cd93a0bb5ba9c0bfbdb3ee114ef90f27be20f7f2
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2025-09-04 23:32:44 +0300

    created repo for tasks

---
+++ Application/Repositories/TaskRepository.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using Db;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories;
+
+public class TaskRepository
+{
+    private readonly DataContext _context;
+
+    public TaskRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Tasks>> GetAllAsync()
+    {
+        return await _context.Tasks.ToListAsync();
+    }
+
+    public async Task<Tasks> GetByIdAsync(Guid id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        return task;
+    }
+
+    public async Task<Tasks> CreateAsync(Tasks task)
+    {
+        await _context.Tasks.AddAsync(task);
+        await _context.SaveChangesAsync();
+        
+        return task;
+    }
+
+    public async Task<Tasks> UpdateAsync(Tasks task)
+    {
+        _context.Tasks.Update(task);
+        await _context.SaveChangesAsync();
+        
+        return task;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var task = await _context.Tasks.FindAsync(id);
+        _context.Tasks.Remove(task);
+        
+        await _context.SaveChangesAsync();
+    }
+}
